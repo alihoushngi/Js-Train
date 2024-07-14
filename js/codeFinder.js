947e08e85ae05f7d4d41b14b66e3756c29ed10ac
alihoushngi <blumy.info@fmail.com>
2024-07-14 14:59:17 +0330

    code finder change

--- js/codeFinder.js
+++ js/codeFinder.js
@@ -1,19 +1,72 @@
 // * find the code wrappers
+// ! get code wrapper section from any component or html file
 const codeWrappers = document.querySelectorAll(".code-wrapper");
+
+// ? create array of codes for analyze
 const data = Array.from(codeWrappers);
+
+// todo regex's for search and find some words
 const variableRegex = /\b(const|var|let|if|else|null|=>)\b/g;
+const ArrayRegex = /\b(Array)\b/g;
+const arrayAttributeRegex = /\b(from|includes|add|replace|forEach|map|test)\b/g;
+const JsAttributeRegex = /\b(.innerHTML|.classList)\b/g;
 const bracketRegex = /(\[\]|\(\))/g;
 const numberRegex = /\d+/g;
 
+// Conditions for adding classes based on comment patterns
+const commentConditions = [
+  { pattern: "// *", className: "code-comment-star" },
+  { pattern: "// !", className: "code-comment-red" },
+  { pattern: "// ?", className: "code-comment-blue" },
+  { pattern: "// todo", className: "code-comment-yellow" },
+  { pattern: "//", className: "code-comment" },
+];
+
+// ! map on data
 data.forEach((data) => {
   const items = Array.from(data.children);
+  // conditional for any attribute to set up color
   items.forEach((tags) => {
-    if (tags.innerHTML.includes("//")) {
-      tags.classList.add("code-comment");
+    const textContent = tags.textContent;
+    const matches = textContent.match(/"([^"]*)"/g);
+    const lines = tags.innerHTML.split("\n");
+    lines.forEach((line) => {
+      const trimmedLine = line.trim();
+      commentConditions.forEach((condition) => {
+        if (trimmedLine.startsWith(condition.pattern)) {
+          tags.classList.add(condition.className);
+        }
+      });
+    });
+    if (matches) {
+      matches.forEach((match) => {
+        if (tags.innerHTML.includes(match)) {
+          const quotes = tags.innerHTML.replace(
+            matches,
+            "<span class='code-strings'>$&</span>"
+          );
+          tags.innerHTML = quotes;
+        }
+      });
     }
-    if (tags.innerHTML.includes("//")) {
-      tags.classList.add("code-comment");
+    if (tags.innerHTML.includes("Array")) {
+      const arrays = tags.innerHTML.replace(
+        ArrayRegex,
+        "<span class='code-array'>$&</span>"
+      );
+      tags.innerHTML = arrays;
     }
+    if (
+      tags.innerHTML.includes(".innerHTML") ||
+      tags.innerHTML.includes(".classList")
+    ) {
+      const jsAttributes = tags.innerHTML.replace(
+        JsAttributeRegex,
+        "<span class='code-jsAttribute'>$&</span>"
+      );
+      tags.innerHTML = jsAttributes;
+    }
+
     if (
       tags.innerHTML.includes("const") ||
       tags.innerHTML.includes("var") ||
@@ -28,6 +81,21 @@ data.forEach((data) => {
         "<span class='code-variable'>$&</span>"
       );
       tags.innerHTML = variables;
+    }
+    if (
+      tags.innerHTML.includes(".from") ||
+      tags.innerHTML.includes(".includes") ||
+      tags.innerHTML.includes(".add") ||
+      tags.innerHTML.includes(".replace") ||
+      tags.innerHTML.includes(".forEach") ||
+      tags.innerHTML.includes(".map") ||
+      tags.innerHTML.includes(".test")
+    ) {
+      const arrayAttributes = tags.innerHTML.replace(
+        arrayAttributeRegex,
+        "<span class='code-arrayAttribute'>$&</span>"
+      );
+      tags.innerHTML = arrayAttributes;
     }
     if (
       tags.innerHTML.includes("[") ||
