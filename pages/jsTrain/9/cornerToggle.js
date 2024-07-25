e08cc0a8343bad072b8f08024e4b2ca2497fd4c1
alihoushngi <blumy.info@fmail.com>
2024-07-26 03:20:03 +0330

    toggle menu corner done

---
+++ pages/jsTrain/9/cornerToggle.js
@@ -0,0 +1,28 @@
+// ? get element from dom
+const toggleButton = document.querySelector("#toggleButton");
+const menuItems = document.querySelectorAll(".corner-menu a");
+
+// ! create state
+let isOpen = false;
+
+// todo toggle control
+toggleButton.addEventListener("click", () => {
+  isOpen = !isOpen;
+  if (isOpen) {
+    toggleButton.classList.add("toggleActive");
+    menuItems[0].style.transform = "translate(12px , -50px)";
+    menuItems[1].style.transform = "translate(48px , -35px)";
+    menuItems[2].style.transform = "translate(62px , 2px)";
+    menuItems[3].style.transform = "translate(46px , 38px)";
+    menuItems[4].style.transform = "translate(9px , 50px)";
+    menuItems[5].style.transform = "translate(-27px , 33px)";
+  } else {
+    toggleButton.classList.remove("toggleActive");
+    menuItems[0].style.transform = "translate(0px , 0px)";
+    menuItems[1].style.transform = "translate(0px , 0px)";
+    menuItems[2].style.transform = "translate(0px , 0px)";
+    menuItems[3].style.transform = "translate(0px , 0px)";
+    menuItems[4].style.transform = "translate(0px , 0px)";
+    menuItems[5].style.transform = "translate(0px , 0px)";
+  }
+});
