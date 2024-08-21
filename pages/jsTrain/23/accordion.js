8cfaa324042e116fee0ca8d54cec3b840391d7d4
alihoushngi <blumy.info@fmail.com>
2024-08-21 18:48:57 +0330

    accordion toggle animation added

--- pages/jsTrain/23/accordion.js
+++ pages/jsTrain/23/accordion.js
@@ -7,11 +7,24 @@ const answers = $.querySelectorAll(".accordion-list-item-answer");
 questions.forEach((question) => {
   question.addEventListener("click", (e) => {
     answers.forEach((answer) => {
-      answer.style.display = "none";
-      answer.style.transform = "scale(0)";
-      e.target.dataset.question == answer.dataset.answer &&
-        ((answer.style.display = "block"),
-        (answer.style.transform = "scale(1)"));
+      // * set style for close all ( if a accordion is open )
+      answer.style.height = "0";
+      answer.style.padding = "0";
+      answer.style.overflow = "hidden";
+
+      // todo check if answer have active class , close it and if not open it
+      if (!answer.classList.contains("active")) {
+        e.target.dataset.question == answer.dataset.answer &&
+          ((answer.style.height = `${answer.scrollHeight + 25}px`),
+          (answer.style.padding = "1rem"),
+          (answer.style.overflow = "visible"),
+          answer.classList.add("active"));
+      } else {
+        answer.classList.remove("active");
+        answer.style.height = "0";
+        answer.style.padding = "0";
+        answer.style.overflow = "hidden";
+      }
     });
   });
 });
